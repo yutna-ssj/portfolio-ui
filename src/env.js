9178ddd0f7248e82fdb4e02e62cbef87ef2696e9
Yuttana Soodsangjan <yuttana.ssj@gmail.com>
2021-11-15 18:43:35 +0700

    update env.js

--- src/env.js
+++ src/env.js
@@ -5,8 +5,8 @@ export const DeployStatus = {
 }
 
 const env_config = {
-    DEV: { url: 'http://192.168.212.122:8080', status: DeployStatus.DEV },
-    PRODUCTION: { url: 'https://yuttana-portfolio-api.herokuapp.com', status: DeployStatus.PRODUCTION }
+    DEV: { url: 'http://192.168.1.103:8080', status: DeployStatus.DEV },
+    PRODUCTION: { url: 'https://yuttana-portfolio.herokuapp.com', status: DeployStatus.PRODUCTION }
 }
 
 export const env = env_config.PRODUCTION;
